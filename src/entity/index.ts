7f8f9abf77709ebc3c1579be74434abc920fe05e
Michael Curry <michaelcurry95@gmail.com>
2021-09-03 20:51:05 -0400

    index-ify entities

---
+++ src/entity/index.ts
@@ -0,0 +1,5 @@
+export { User } from './User';
+export { Exercise } from './Exercise';
+export { ExerciseType } from './ExerciseType';
+export { Workout } from './Workout';
+export { Cardio } from './Cardio';
